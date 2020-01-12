126d553c0fabadad3ce117b804eae2b77fb765a5
Kristijan Cosic <kristijan.cosic@i-grupa.com>
2020-01-12 20:14:40 +0100

    Colors, shaders, movement
    Changed colors.
    Added shaders for enemies.
    Added particles for enemies when they die.
    Added movement to player.

--- Assets/Scripts/Enemy/EnemyBloodAnimationController.cs
+++ Assets/Scripts/Enemy/EnemyBloodAnimationController.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyBloodAnimationController : MonoBehaviour {
 
     [SerializeField]
-    private GameObject _particlePrefab;
+    private GameObject _shotParticlePrefab, _diedParticlePrefab;
     private GameObjectEventManager _gameObjectEventManager;
     private Transform _transform;
     
 	void Start () {
         _gameObjectEventManager = GetComponent<GameObjectEventManager>();
-        _gameObjectEventManager.StartListening("Shot", StartAnimation);
+        _gameObjectEventManager.StartListening("Shot", StartShotAnimation);
+        _gameObjectEventManager.StartListening("Died", StartDiedAnimation);
         _transform = GetComponent<Transform>();
 	}
-	
-    void StartAnimation(string vectorJson)
+
+    private void StartDiedAnimation()
+    {
+        var particleSystem = Instantiate(_diedParticlePrefab, _transform.position, Quaternion.identity);
+        particleSystem.GetComponent<Transform>().rotation = _transform.rotation;
+    }
+
+    void StartShotAnimation(string vectorJson)
     {
         Vector3 position = JsonUtility.FromJson<ShootInfo>(vectorJson).pointOfHit;
-        Instantiate(_particlePrefab, position, Quaternion.identity, _transform);
-        //_particlePrefab.GetComponent<ParticleSystem>().Play();
+        var particleSystem = Instantiate(_shotParticlePrefab, position, Quaternion.identity, _transform);
+        particleSystem.GetComponent<Transform>().localRotation = new Quaternion(0, 0, 0, 0);
     }
 }
---
+++ Assets/Scripts/Player/PositionController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionController : MonoBehaviour {
+
+    [SerializeField]
+    private List<Transform> _movePointsList;
+    private Queue<Transform> _movePoints;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private AnimationCurve _animationCurve;
+    private float _timer = 1f;
+    private Transform _transform;
+    private Transform _goalTransform, _currentTransform;
+
+	void Start () {
+        _movePoints = new Queue<Transform>();
+        foreach(var point in _movePointsList)
+        {
+            _movePoints.Enqueue(point);
+        }
+        _transform = GetComponent<Transform>();
+        _goalTransform = _transform;
+        _currentTransform = _transform;
+        EventManager.StartListening("MoveToNextPoint", MoveToNextPoint);
+	}
+
+    private void MoveToNextPoint()
+    {
+        if(_movePoints.Count > 0)
+        {
+            _timer = 0f;
+            _currentTransform = _goalTransform;
+            _goalTransform = _movePoints.Dequeue();
+            Debug.Log(_goalTransform.position);
+        }
+    }
+
+    void Update () {
+        //TODO remove after testing
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            EventManager.TriggerEvent("MoveToNextPoint");
+        }
+
+        _transform.position = Vector3.Lerp(_currentTransform.position, _goalTransform.position, _animationCurve.Evaluate(_timer));
+        _transform.rotation = Quaternion.Lerp(_currentTransform.rotation, _goalTransform.rotation, _animationCurve.Evaluate(_timer));
+        if(_timer < 1f)
+        {
+            _timer += _speed * CustomTime.GetDeltaTime();
+        }
+        else if(_timer > 1f)
+        {
+            _timer = 1f;
+            _transform.position = _goalTransform.position;
+            _transform.rotation = _goalTransform.rotation;
+        }
+	}
+}
